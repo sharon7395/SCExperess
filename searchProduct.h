21958c19dd90650eb42a2fe9268092c4d812a51e
Sharon_Angdo <94113050+sharon7395@users.noreply.github.com>
2022-05-02 14:44:09 +0300

    Delete searchProduct.h

--- searchProduct.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Product.h"
-
-int Name_search(Product* list, int size, char* name);
-int Serial_num_search(Product* list, int size, int num);
-void Print_Rating(int total_rate, int rating_count);
-void Website_ranking(int* total, int* count);
-void Low_to_high(Product* list, int size);
-void Select_cat(Product* list, int size);
